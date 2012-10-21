1991121c9b5084ee0e936f46497518817a3ce87a
Anthony Le Courtes <anthony.lecourtes@gmail.com>
2012-10-21 17:48:12 +0200

    Ajout de commentaires & gestion images offline
    Utilisation de offline.jpg

---
+++ js/offline.js
@@ -0,0 +1,1 @@
+alert("Vous êtes actuellement hors ligne, certaines fonctionnalités ne sont plus disponible !");
--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ function  onUpdateReady() {
 	alert('Nouvelle version de cache à jour');
 }
 
+//Utilisation de l'object window.applicationCache pour piloter le cache
+
 window.applicationCache.addEventListener('updateready', onUpdateReady);
 
 if(window.applicationCache.status == window.applicationCache.UPDATEREADY){
